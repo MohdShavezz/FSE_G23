f2a0720b0622170bb2e52ebf65c21b6ef1991d64
MohdShavezz <mohdpctebtech19cse@gmail.com>
2026-05-06 15:32:09 +0530

    react typescript Counter App Form Handling Higher Order Component

---
+++ Lec_11_May_06_React_With_TS/src/App.tsx
@@ -0,0 +1,16 @@
+import UserForm from './components/UserForm'
+import Wrapper from './components/Wrapper'
+import Comp from './components/Comp'
+
+const App = () => {
+  return (
+    <div>
+      <Wrapper>
+          <Comp/>
+      </Wrapper>      
+      <UserForm/>
+    </div>
+  )
+}
+
+export default App
